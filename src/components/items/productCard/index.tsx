df275d069bca71ed50f5657ca043b55f8c034c24
WissamKahwaji <kahwajiwisam@gmail.com>
2023-12-11 11:03:31 +0400

    edits

--- src/components/items/productCard/index.tsx
+++ src/components/items/productCard/index.tsx
@@ -13,8 +13,14 @@ const ProductCard = ({
 }: ProductCardProps) => {
   return (
     <Link to={`/product/${id}`}>
-      <div className="flex w-[250px] flex-col items-center gap-4 overflow-hidden transition-transform hover:scale-105 sm:w-[280px] sm:gap-5 md:w-[320px] ">
-        <div className=" w-full ">
+      <div className="  group flex w-[250px] flex-col items-center gap-4 overflow-hidden sm:w-[280px] sm:gap-5 md:w-[320px] ">
+        <div className="  relative w-full ">
+          <div className="absolute left-0 top-0 flex h-0 w-full items-center justify-center bg-border/50  transition-all duration-500 group-hover:h-full  ">
+            <p className=" hidden bg-background text-center opacity-100 group-hover:block">
+              {" "}
+              {desc}
+            </p>
+          </div>
           <img
             className="aspect-square h-full w-full object-cover"
             src={img}
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -12,41 +12,64 @@ const Product = () => {
 
   const { data: productDetails } = useGetProductDetailsQuery(id);
   return (
-    <div className="mx-16 grid  grid-cols-1 gap-8 py-24 sm:mx-20 md:mx-32 md:grid-cols-2 md:gap-16 lg:mx-52">
-      <div>
-        <img
-          className="aspect-square h-full w-full object-contain"
-          src={productDetails?.img}
-          alt={productDetails?.title}
-        />
-      </div>
-      <div className=" flex flex-col gap-4 bg-gray-background px-6 py-4 md:gap-6 ">
-        <p className="sm:2xl lg:5xl border-b border-border pb-4 text-xl font-semibold uppercase md:text-4xl">
+    <div className="mx-16  py-24 sm:mx-24 md:mx-36 lg:mx-96">
+      <div className=" flex flex-col gap-4 bg-gray-background px-6 py-4 md:flex-row md:gap-6 ">
+        <p className="sm:2xl lg:5xl border-b border-border pb-4 text-xl font-semibold uppercase md:hidden md:text-4xl">
           {productDetails?.title}
         </p>
-        <p className="text-lg  sm:text-xl md:text-2xl">
-          {productDetails?.type.name}
-        </p>
-        <p className="flex items-center  text-lg text-muted-foreground sm:text-xl md:text-2xl">
-          <BsCurrencyDollar />
-          <span>{productDetails?.deepDetails.price}</span>
-        </p>
-        <div className="h-32 overflow-hidden lg:h-96">
-          <p>{productDetails?.desc}</p>
+        <div className="h-60 w-60 md:h-[450px] md:w-[450px]">
+          <img
+            className="aspect-square h-full w-full object-cover"
+            src={productDetails?.img}
+            alt={productDetails?.title}
+          />
         </div>
-        <div className="flex items-center justify-center gap-8">
-          <div className="flex items-center justify-center gap-4">
-            <p className="bg-background p-2 px-4">{count}</p>
-            <div className="flex flex-col">
-              <button onClick={() => setCount((count) => count + 1)}>
-                <CiSquarePlus className="h-6 w-6" />
-              </button>
-              <button onClick={() => setCount((count) => count - 1)}>
-                <CiSquareMinus className="h-6 w-6" />
-              </button>
+        <div className="flex flex-col  gap-4 md:gap-6">
+          <p className="sm:2xl lg:5xl hidden border-b border-border pb-4 text-xl font-semibold uppercase md:block md:text-4xl">
+            {productDetails?.title}
+          </p>
+          <div className="flex items-center gap-2">
+            <p className=" font-semibold capitalize   md:text-xl">type : </p>
+            <p className="  sm:text-lg md:text-xl">
+              {productDetails?.type.name}
+            </p>
+          </div>
+          <div className="flex  flex-col gap-4 md:flex-row md:gap-8">
+            <div className="flex  items-center gap-2">
+              <p className=" font-semibold capitalize   md:text-xl">price : </p>
+              <p className="flex items-center   text-muted-foreground sm:text-lg md:text-xl">
+                <BsCurrencyDollar />
+                <span>{productDetails?.deepDetails.price}</span>
+              </p>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className=" font-semibold capitalize   md:text-xl">wight : </p>
+              <p className="flex items-center   text-muted-foreground sm:text-lg md:text-xl">
+                <span>{productDetails?.deepDetails?.weight}</span>
+                <span className="uppercase">kg</span>
+              </p>
             </div>
           </div>
-          <Button> add to card</Button>
+          <div className="overflow-hidden ">
+            <p>{productDetails?.desc}</p>
+          </div>
+          <div className="flex flex-col  gap-8">
+            <div className="flex items-center justify-center gap-4">
+              <p className="border bg-background p-2 px-4 shadow-sm">{count}</p>
+              <div className="flex ">
+                <button
+                  className="transition-transform hover:scale-90"
+                  onClick={() => setCount((count) => count + 1)}
+                >
+                  <CiSquarePlus className="h-12 w-12" />
+                </button>
+                <button onClick={() => setCount((count) => count - 1)}>
+                  <CiSquareMinus className="h-12 w-12" />
+                </button>
+              </div>
+            </div>
+            <Button> add to card</Button>
+          </div>
         </div>
       </div>
     </div>
